a6874d907e718a0a87588087476575a3f23d5a89
Dev-developer69 <devansh123maheshwari123@gmail.com>
2025-04-01 13:35:58 +0530

    tutedude python repository

---
+++ assignment1.py
@@ -0,0 +1,15 @@
+# # TASK 1
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+
+print("Addition :",a+b)
+print("Subtraction :",a-b)
+print("Multiplication :",a*b)
+print("Division :",a/b)
+
+
+# TASK 2
+first_name=input("enter your first name : ")
+last_name=input("enter your last name : ")
+full_name= first_name+ " " + last_name +"!"
+print("hello," ,full_name, " Welcome to the python program")
